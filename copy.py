3f69bff6d58bb02bf37e22c7c43676006d5cb331
PS-anon <60316035+PS-anon@users.noreply.github.com>
2020-06-27 12:40:37 +0300

    Add files via upload

--- copy.py
+++ copy.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 import webbrowser
 from datetime import datetime
 import os
+import pyaudio
 
 r = sr.Recognizer()
 intrebari = 'ce faci'
@@ -20,9 +21,26 @@ with sr.Microphone() as source:
 if "deschide Facebook" in voicee :
     os.system("say -v Ioana 'Deschid Facebook'")
     webbrowser.open('https://www.facebook.com')
-elif "deschide Google" in voicee :
-    os.system("say -v Ioana 'Deschid google'")
-    webbrowser.open('https://google.com')
+elif "caută pe Facebook" in voicee :
+    os.system("say -v Ioana 'ce vrei sa cauti ?'")
+    with sr.Microphone() as source:
+        fb = r.listen(source)
+        os.system("say -v Ioana 'Am priceput'")
+        try:
+            fac = r.recognize_google(fb, language = "ro-RO")
+            webbrowser.open("https://www.facebook.com/search/top/?q=" + fac)
+        except:
+             os.system("say -v Ioana 'Ok am o problema , incerc sa o repar , reincearca in 10 minute !'")
+elif "caută pe Google" in voicee :
+    os.system("say -v Ioana 'ce vrei sa cauti ?'")
+    with sr.Microphone() as source:
+        gog = r.listen(source)
+        os.system("say -v Ioana 'Am priceput'")
+        try:
+            gooogle = r.recognize_google(gog, language = "ro-RO")
+            webbrowser.open("https://www.google.com/" + gooogle)
+        except:
+             os.system("say -v Ioana 'Ok am o problema , incerc sa o repar , reincearca in 10 minute !'")
 elif "Vreau să ascult piesa favorită" in voicee :
     os.system("say -v Ioana 'Niste Ombladon e  pe drum hehe'")
     webbrowser.open('https://www.youtube.com/watch?v=ektftzuw91A')
@@ -36,6 +54,49 @@ elif raspunsuri_ok in voicee :
     os.system("say -v Ioana 'Blana'")
 elif raspunsuri_grs in voicee:
     os.system("say -v Ioana 'Hmm nu e ok '")
+elif "deschide Atom" in voicee :
+    os.system("say -v Ioana 'Ok deschid atom'")
+    os.system("atom")
+elif "deschide website" in voicee :
+    os.system("say -v Ioana 'Ok, scrie denumirea website-tului'")
+    inp = input("-> ")
+    os.system("say -v Ioana 'Acum se deschide'")
+elif "deschide Youtube" in voicee :
+    os.system("say -v Ioana deschid youtube")
+    webbrowser.open("https://www.youtube.com")
+elif "caută pe Youtube" in voicee :
+    os.system("say -v Ioana 'ce vrei sa cauti ?'")
+    with sr.Microphone() as source:
+        yt = r.listen(source)
+        os.system("say -v Ioana 'Am priceput'")
+        try:
+            yut = r.recognize_google(yt, language = "ro-RO")
+            webbrowser.open("https://www.youtube.com/results?search_query=" + yut)
+        except:
+             os.system("say -v Ioana 'Ok am o problema , incerc sa o repar , reincearca in 10 minute !'")
+elif "caută pe Wikipedia" in voicee :
+    os.system("say -v Ioana 'ce vrei sa cauti ?'")
+    with sr.Microphone() as source:
+        wik = r.listen(source)
+        os.system("say -v Ioana 'Am priceput'")
+        try:
+            wikkk = r.recognize_google(wik, language = "ro-RO")
+            webbrowser.open("https://en.wikipedia.org/wiki/" + wikkk)
+        except:
+             os.system("say -v Ioana 'Ok am o problema , incerc sa o repar , reincearca in 10 minute !'")
+
+elif "pune muzică" in voicee :
+    os.system("say -v Ioana 'Ce vrei sa asculti ? '")
+    with sr.Microphone() as source:
+        muz = r.listen(source)
+        os.system("say -v Ioana 'Am priceput'")
+        try:
+            muzi = r.recognize_google(muz, language = "ro-RO")
+            os.system(f"youtube-dl -o 'muzica.%(ext)s' 'ytsearch1:{muzi}'")
+            os.system("open muzica.mp4")
+        except:
+             os.system("say -v Ioana 'Ok am o problema , incerc sa o repar , reincearca in 10 minute !'")
+    
 else :
     os.system("say -v Ioana 'Nu am inteles'")
-    
+   
